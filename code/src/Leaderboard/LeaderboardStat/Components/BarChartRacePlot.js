bc87d45344d088961956c0f6d9c2e51ecd52ef67
Taha <tahanaeem12@hotmail.com>
2021-03-19 10:07:04 -0400

    bars are sorted and transition

--- code/src/Leaderboard/LeaderboardStat/Components/BarChartRacePlot.js
+++ code/src/Leaderboard/LeaderboardStat/Components/BarChartRacePlot.js
@@ -39,49 +39,62 @@ const BarChart = ({
   for (let i = 0; i < y.length; i++) {
     for (let j = 0; j < maxlength; j++) {
       if (j < y[i].length) {
-        pointsData[j].push(y[i][j]);
+        let obj = {
+          name: text[i],
+          value: y[i][j],
+        };
+        pointsData[j].push(obj);
       } else {
-        pointsData[j].push(0);
+        let obj = {
+          name: text[i],
+          value: 0,
+        };
+        pointsData[j].push(obj);
       }
     }
   }
   console.log(pointsData);
+  console.log(text);
+  console.log(y);
   useEffect(() => {
     const svg = select(svgRef.current);
-    //console.log(gameIndex);
+    pointsData[gameIndex].sort((a, b) => b.value - a.value);
     const yScale = scaleBand()
-      .paddingInner(0.1)
+      .paddingInner(0.3)
       .domain(pointsData[gameIndex].map((value, index) => index))
-      .range([0, 200]);
+      .range([0, 400]);
     console.log(max(pointsData[gameIndex]));
     const xScale = scaleLinear()
-      .domain([0, max(pointsData[gameIndex])])
+      .domain([0, max(pointsData[gameIndex], (entry) => entry.value)])
       .range([0, 900]);
 
     svg
+
       .selectAll(".bar")
-      .data(pointsData[gameIndex])
+      .data(pointsData[gameIndex], (entry) => entry.name)
       .join("rect")
       .attr("fill", (entry, index) => teamColours[index])
       .attr("class", "bar")
       .attr("x", 0)
-      .attr("width", (entry) => xScale(entry))
+      .transition()
+      .attr("width", (entry) => xScale(entry.value))
       .attr("y", (entry, index) => yScale(index))
       .attr("height", yScale.bandwidth());
+
     svg
       .selectAll(".label")
-      .data(pointsData[gameIndex], (entry, index) => text[index])
+      .data(pointsData[gameIndex], (entry) => entry.name)
       .join("text")
-      .attr("fill", "#FFFFFF")
-      .text((entry, index) => `${text[index]} Points ${entry}`)
+      .attr("fill", "#aaaaaa")
+      .text((entry) => `${entry.name} Points ${entry.value}`)
       .attr("class", "label")
       .attr("x", 10)
       .attr("y", (entry, index) => yScale(index) + yScale.bandwidth());
-  }, [gameIndex]);
+  });
 
   return (
     <div>
-      <svg width="100%" height="300" ref={svgRef}></svg>
+      <svg width="100%" height="450" ref={svgRef}></svg>
       <button
         style={{ color: "red" }}
         onClick={() => setGameIndex(gameIndex + 1)}
