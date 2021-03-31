baa10477fe26fa574f77b95688ad02ebec6a3c02
Taha <tahanaeem12@hotmail.com>
2021-03-31 18:12:57 -0400

    frames per game adjust based on min&max values

--- code/src/Leaderboard/LeaderboardStat/Components/BarChartRacePlot.js
+++ code/src/Leaderboard/LeaderboardStat/Components/BarChartRacePlot.js
@@ -32,8 +32,9 @@ const BarChart = ({
   const [play, setPlay] = useState(false);
   const [timerID, setTimerID] = useState(null);
   const l = 50;
-  const [incr, setIncr] = useState(l - 1);
-
+  const [incr, setIncr] = useState(0);
+  let currentTeamFrame = 0;
+  let numOfTeams = y.length;
   let maxlength = 0;
   // finds highest number of games
   y.forEach((element) => {
@@ -67,33 +68,55 @@ const BarChart = ({
   // console.log(pointsData);
   // console.log(text);
   // console.log(y);
-
+  //console.log(pointsData);
   //console.log(pointsData[0]);
+  let maxVal = 0;
   for (let i = 0; i < pointsData.length; i++) {
+    let maxcurrent = max(pointsData[i], (entry) => entry.value);
+    let mincurrent = min(pointsData[i], (entry) => entry.value);
+    if (maxcurrent - mincurrent > maxVal) {
+      maxVal = maxcurrent - mincurrent;
+      //console.log(i);
+    }
+  }
+
+  let lastFrameDiff = maxVal;
+  // let lastFrameDiff =
+  //   min(pointsData[pointsData.length - 1], (entry) => entry.value) -
+  //   max(pointsData[pointsData.length - 1], (entry) => entry.value);
+
+  //console.log("difference = " + maxVal);
+  for (let i = 0; i < pointsData.length; i++) {
+    let currentFrameDiff =
+      max(pointsData[i], (entry) => entry.value) -
+      min(pointsData[i], (entry) => entry.value);
     for (let j = 0; j < pointsData[i].length; j++) {
       let team = pointsData[i][j];
       let prevValue = 0;
       let nextValue = team.value;
+
       if (i > 0) {
         let prevTeam = pointsData[i - 1][j];
         prevValue = prevTeam.value[prevTeam.value.length - 1];
       }
-      const increment = (nextValue - prevValue) / l;
+      const increment =
+        (nextValue - prevValue) / (l * (currentFrameDiff / lastFrameDiff));
       const newValues = [];
       newValues.push(prevValue);
-      for (let k = 0; k < l - 2; k++) {
+      for (let k = 0; k < (l * currentFrameDiff) / lastFrameDiff - 2; k++) {
         newValues.push(increment * (k + 1) + prevValue);
       }
       newValues.push(nextValue);
       team.value = newValues;
     }
   }
+
   console.log(pointsData);
   // console.log("Gameindex = " + gameIndex);
   // console.log("Incr = " + incr);
   useEffect(() => {
     setGameIndex(0);
-    setIncr(l - 1);
+    //setIncr(pointsData[gameIndex][0].value.length - 1);
   }, [y]);
   // useEffect(() => {
   //   if (play) {
@@ -110,6 +133,7 @@ const BarChart = ({
   // }, [play]);
 
   useEffect(() => {
+    //console.log("update");
     if (gameIndex + 1 > pointsData.length - 1) {
       clearInterval(timerID);
     }
@@ -126,7 +150,7 @@ const BarChart = ({
       const xScale = scaleLinear()
         .domain([0, max(pointsData[gameIndex], (entry) => entry.value[incr])])
         .range([0, select(widthDivRef).node().current.clientWidth]);
-      console.log();
+
       svg
         .selectAll(".bar")
         .data(pointsData[gameIndex], (entry) => entry.name)
@@ -136,22 +160,43 @@ const BarChart = ({
         .attr("x", 0)
         .attr("fill-opacity", 0.7)
         .transition()
-        .duration(200)
+        .duration(300)
         .on("end", function () {
           if (play) {
-            if (incr < l - 2) {
+            currentTeamFrame += 1;
+
+            if (
+              incr < pointsData[gameIndex][0].value.length - 1 &&
+              currentTeamFrame >= numOfTeams
+            ) {
+              console.log("Frame " + incr);
+              console.log("GameIndex: " + gameIndex);
+              console.log("Length: " + pointsData[gameIndex][0].value.length);
+              currentTeamFrame = 0;
               setIncr((incr) => incr + 1);
-            } else if (gameIndex < pointsData.length - 1) {
+            } else if (
+              gameIndex < pointsData.length - 1 &&
+              currentTeamFrame >= numOfTeams
+            ) {
               setIncr(0);
               setGameIndex(gameIndex + 1);
+              currentTeamFrame = 0;
+
+              // console.log(gameIndex);
+              // console.log(incr);
             }
           }
         })
         .ease(easeLinear)
-        .attr("width", (entry, index) => xScale(entry.value[incr]))
+        .attr("width", function (entry, index) {
+          console.log("------Game index: " + gameIndex);
+          console.log("------Frame: " + incr);
+          console.log("------Length: " + pointsData[gameIndex][0].value.length);
+          return xScale(entry.value[incr]);
+        })
         .attr("y", (entry, index) => yScale(index))
         .attr("height", yScale.bandwidth());
-      // console.log(teamColours);
+      //console.log(teamColours);
 
       svg
         .selectAll(".label")
@@ -163,7 +208,7 @@ const BarChart = ({
             `${entry.name} ${Math.round(entry.value[incr] * 100) / 100}`
         )
         .transition()
-        .duration(200)
+        .duration(300)
         .attr("class", "label")
         .attr("x", 10)
         .attr(
@@ -173,10 +218,16 @@ const BarChart = ({
       //console.log("doing stuf");s
     }
   });
-  console.log(isTotal);
+  //console.log(incr);
   const handleOnChange = (e) => {
     setGameIndex(e.target.value - 1);
-    setIncr(l - 1);
+    //console.log(gameIndex);
+    let currentIndex = (gameIndex) => gameIndex;
+    setIncr(pointsData[gameIndex][0].value.length - 3);
+    currentTeamFrame = 0;
+    // console.log(gameIndex);
+    // console.log(pointsData[gameIndex][0].value.length - 1);
+    console.log(incr);
   };
 
   return (
