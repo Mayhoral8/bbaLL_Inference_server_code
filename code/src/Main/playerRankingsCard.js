b9a9e38e09e9c65caa8e790b9cd933e0ace0fc17
Taha <tahanaeem12@hotmail.com>
2021-05-02 06:21:22 -0400

    player images now load

--- code/src/Main/playerRankingsCard.js
+++ code/src/Main/playerRankingsCard.js
@@ -5,7 +5,7 @@ import { CardContainer } from "./playerrankingscard-style";
 
 const PlayerRankingsCard = ({ data }) => {
   //const [JSON, setJSON] = useState(data);
-  const [imgs, setImgs] = useState();
+  const [imgs, setImgs] = useState("");
 
   try {
     if (data == null) {
@@ -32,7 +32,6 @@ const PlayerRankingsCard = ({ data }) => {
   const hasDataLoaded = Object.entries(data[0]).length !== 0;
   if (hasDataLoaded) {
     useEffect(() => {
-      console.log(data[rankingTypeIndex][scoreType.current]);
       setTopPlayers(
         data[rankingTypeIndex][scoreType.current]
           .sort((a, b) => {
@@ -44,62 +43,92 @@ const PlayerRankingsCard = ({ data }) => {
       );
     }, []);
   }
+
+  useEffect(() => {
+    let names = [];
+    Object.values(topPlayers).map((obj) => names.push(Object.keys(obj)[0]));
+    getPic(names);
+  }, [topPlayers]);
+  console.log(imgs);
+
+  const getPic = async (playerNames) => {
+    playerNames.map((name) => {
+      const imageReference = fbStorage.refFromURL(
+        "gs://nba-database-cb52a.appspot.com/player_photo_hayaoStyle_v2/" +
+          name.replaceAll(" ", "_") +
+          ".png"
+      );
+      imageReference.getDownloadURL().then((url) => {
+        setImgs((imgs) => [...imgs, url]);
+      });
+    });
+  };
+
   const renderComponent = () => {
     if (hasDataLoaded == true) {
       return (
-        <CardContainer>
-          <div className="top">
-            <div className="title">
-              {rankingTypes[rankingTypeIndex]} Player Rankings
+        <div>
+          <button>
+            <img
+              style={{ maxWidth: "40px" }}
+              src="https://image.flaticon.com/icons/png/512/60/60758.png"
+            />
+          </button>
+          <CardContainer>
+            <div className="top">
+              <div className="title">
+                {rankingTypes[rankingTypeIndex]} Player Rankings
+              </div>
+
+              <div className="dropdown">
+                <select
+                  onChange={(e) => {
+                    scoreType.current = e.target.value;
+
+                    setTopPlayers(
+                      data[rankingTypeIndex][scoreType.current]
+                        .sort((a, b) => {
+                          const aName = Object.keys(a)[0];
+                          const bName = Object.keys(b)[0];
+                          return a[aName]["Rank"] - b[bName]["Rank"];
+                        })
+                        .slice(0, 4)
+                    );
+                  }}
+                  value={scoreType.current}
+                >
+                  {Object.keys(data[rankingTypeIndex]).map((scoreTypes) => {
+                    return <option value={scoreTypes}>{scoreTypes}</option>;
+                  })}
+                </select>
+              </div>
             </div>
 
-            <div className="dropdown">
-              <select
-                onChange={(e) => {
-                  scoreType.current = e.target.value;
-
-                  setTopPlayers(
-                    data[rankingTypeIndex][scoreType.current]
-                      .sort((a, b) => {
-                        const aName = Object.keys(a)[0];
-                        const bName = Object.keys(b)[0];
-                        return a[aName]["Rank"] - b[bName]["Rank"];
-                      })
-                      .slice(0, 4)
-                  );
-                }}
-                value={scoreType.current}
-              >
-                {Object.keys(data[rankingTypeIndex]).map((scoreTypes) => {
-                  return <option value={scoreTypes}>{scoreTypes}</option>;
-                })}
-              </select>
-            </div>
-          </div>
-
-          <div className="players">
-            {Object.keys(topPlayers).map((key) => {
-              const playerName = Object.keys(topPlayers[key])[0];
-              return (
-                <div className="player-box">
-                  <div className="logo-box">
-                    <div className="player-name">
-                      {playerName.split(" ")[0]} <br />
-                      {playerName.split(" ")[1]}
+            <div className="players">
+              {Object.keys(topPlayers).map((key, index) => {
+                const playerName = Object.keys(topPlayers[key])[0];
+                return (
+                  <div className="player-box">
+                    <div className="logo-box">
+                      <img src={imgs[index]} />
+                      <div className="player-name">
+                        {playerName.split(" ")[0]} <br />
+                        {playerName.split(" ")[1]}
+                      </div>
+                    </div>
+                    <div className="value">
+                      {Math.round(
+                        topPlayers[key][playerName][
+                          valueType[scoreType.current]
+                        ] * 100
+                      ) / 100}
                     </div>
                   </div>
-                  <div className="value">
-                    {Math.round(
-                      topPlayers[key][playerName][
-                        valueType[scoreType.current]
-                      ] * 100
-                    ) / 100}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </CardContainer>
+                );
+              })}
+            </div>
+          </CardContainer>
+        </div>
       );
     } else {
       return (
@@ -113,14 +142,4 @@ const PlayerRankingsCard = ({ data }) => {
   return renderComponent();
 };
 
-// const GetPlayerImage = (playerName) => {
-//   const imageReference = fbStorage.refFromURL(
-//     "gs://nba-database-cb52a.appspot.com/player_photo_hayaoStyle_v2/" +
-//       playerName.replaceAll(" ", "_") +
-//       ".png"
-//   );
-//   imageReference.getDownloadURL().then((url) => {
-//     return url;
-//   });
-// };
 export default PlayerRankingsCard;
