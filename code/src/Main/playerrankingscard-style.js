3c5b87728bc5bb0ee8f8bf72b9050b37631421ef
Taha <tahanaeem12@hotmail.com>
2021-05-14 17:11:42 -0400

    changed margins for mobile

--- code/src/Main/playerrankingscard-style.js
+++ code/src/Main/playerrankingscard-style.js
@@ -6,6 +6,10 @@ export const OutsideContainer = styled.div`
     left: -2.7rem;
   }
 
+  @media (max-width: 834px) {
+    margin: 2rem 0rem 2rem 0rem;
+  }
+
   margin-left: 0rem;
   margin-top: 0rem;
   margin-bottom: auto;
@@ -28,9 +32,16 @@ export const CardContainer = styled.div`
   border: solid gray 1px;
   display: inline-block;
   flex-direction: column;
+
+  @media (max-width: 834px) {
+    min-width: 280px;
+  }
+
   min-width: 470px;
+  max-width: 470px;
   max-height: 500px;
 
+  width: 100%;
   border-radius: 5px;
   padding: 1rem;
 
@@ -39,6 +50,9 @@ export const CardContainer = styled.div`
     flex-direction: row;
     justify-content: space-between;
     margin-bottom: 0.5rem;
+    @media (max-width: 834px) {
+      font-size: 1rem;
+    }
     font-size: 1.5rem;
   }
   .title {
@@ -53,9 +67,15 @@ export const CardContainer = styled.div`
 
   .dropdown {
     margin: 0rem 0rem 0rem auto;
+    @media (max-width: 834px) {
+      width: 45%;
+    }
     width: 35%;
   }
   .select {
+    @media (max-width: 834px) {
+      font-size: 0.7rem;
+    }
     font-size: 0.9rem;
   }
 
@@ -65,13 +85,22 @@ export const CardContainer = styled.div`
     justify-content: space-between;
 
     //background: #c4c4c4;
+
+    @media (max-width: 834px) {
+      padding: 0.25rem;
+      margin-top: 0.25rem;
+      margin-bottom: 0.25rem;
+    }
     margin-top: 0.5rem;
     margin-bottom: 0.5rem;
     padding: 0.5rem;
+
     border-radius: 5px;
     //color: #e0e0e0;
     color: #2e2e2e;
-
+    @media (max-width: 834px) {
+      max-height: 60px;
+    }
     max-height: 85px;
   }
 
@@ -97,6 +126,9 @@ export const CardContainer = styled.div`
 
     font-family: Roboto;
     font-weight: 100;
+    @media (max-width: 834px) {
+      font-size: 0.8rem;
+    }
     font-size: 1.3rem;
   }
 
@@ -108,10 +140,16 @@ export const CardContainer = styled.div`
     align-items: center; //vertical align;
     font-family: Roboto Condensed;
     font-weight: 900;
+    @media (max-width: 834px) {
+      font-size: 1.5rem;
+    }
     font-size: 2.5rem;
   }
 
   .unit {
+    @media (max-width: 834px) {
+      font-size: 0.7rem;
+    }
     font-size: 1rem;
   }
 `;
