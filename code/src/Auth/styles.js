34bf6457a10bbba9e3f7f356c22d3b3f86f5c307
Taha <tahanaeem12@hotmail.com>
2021-05-17 19:24:48 -0400

    new files

---
+++ code/src/Auth/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const LoginContainer=styled.div`
+    width:100%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+export const LoginWrapper=styled.div`
+    height:500px;
+    width: 300px;
+    box-shadow: 0px 0px 5px rgba(179, 179, 179, 0.9);
+    background:#404040;
+`
+export const LoginHeader=styled.div`
+    padding:0px 20px;
+`
+export const LoginHeaderWrapper=styled.div`
+    width:100%;
+    padding:20px 0px;
+`
+export const Heading=styled.h2`
+    color:#b3b3b3;
+`
+export const ContentContainer=styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    height:430px;
+`
+export const ButtonContainer=styled.div`
+    width:90%;
+    background: #ffff;
+    display:flex;
+    align-items:center;
+    margin:10px 0px;
+    padding:5px 0px;
+    padding-left:10px;
+    border-radius:7px;
+    cursor: pointer;
+`
+export const Button=styled.button`
+    width:100%;
+    text-align:start;
+    font-size:17px;
+    color:#b3b3b3;
+`
+export const Image=styled.img`
+    height:30px;
+    width:30px;
+    margin-right:10px;
+`
---
+++ code/src/betting/Shared/bettingSectionHeaderStyles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+export const BettingSectionheaderContainer=styled.div`
+    width:100%;
+    padding:10px;
+    display:flex;
+    background: rgba(0, 0, 0, 0.05);
+`
+
+
+export const HeaderSection1=styled.div`
+    max-width: 20%;
+    min-width: 258px;
+    color: black;
+    font-weight: 900;
+`
+
+export const HeaderSection2=styled.div`
+    max-width:80%;
+    min-width: calc(100% - 258px);
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+`
+
+export const HeaderChildSection=styled.div`
+    text-align:center;
+    width:33.33%;
+    color: black;
+    font-weight: 900;
+`
