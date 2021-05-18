923994e0c447a193d3c8fa7c075a7ded54ea523c
Jiwoong Im <jiwoongim@a-10-27-20-252.dynapool.vpn.nyu.edu>
2021-05-18 18:46:10 -0400

    routes fixed

--- code/src/App/Routes.js
+++ code/src/App/Routes.js
@@ -17,92 +17,61 @@ import "firebase/auth";
 import { firebaseInstanceSpigamebet } from "./spigamebetFirebase";
 import { checkLoginStatus } from "../redux/actions/authActions";
 const Games = Loadable({
-    loader: () => import("../GameStats/GamePageContainer"),
-    loading() {
-      return <Spinner />;
-    },
-  });
-  const Leaderboard = Loadable({
-    loader: () => import("../Leaderboard/LeaderPageContainer"),
-    loading() {
-      return <Spinner />;
-    },
-  });
-  const Stats = Loadable({
-    loader: () => import("../Stats/StatsPageContainer.js"),
-    loading() {
-      return <Spinner />;
-    },
-  });
-  const Indiv = Loadable({
-    loader: () => import("../Individual/IndivContainer"),
-    loading() {
-      return <Spinner />;
-    },
-  });
-  const Comparsion = Loadable({
-    loader: () => import("../Comparison/ComparisonPage"),
-    loading() {
-      return <Spinner />;
-    },
-  });
-  const Betting = Loadable({
-    loader: () => import("../betting/index"),
-    loading() {
-      return <Spinner />;
-    },
-  });
-  const ProfilePage = Loadable({
-    loader: () => import("../Profilepage/index"),
-    loading() {
-      return <Spinner />;
-    },
-  });
+  loader: () => import("../GameStats/GamePageContainer"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const Leaderboard = Loadable({
+  loader: () => import("../Leaderboard/LeaderPageContainer"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const Stats = Loadable({
+  loader: () => import("../Stats/StatsPageContainer.js"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const Indiv = Loadable({
+  loader: () => import("../Individual/IndivContainer"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const Comparsion = Loadable({
+  loader: () => import("../Comparison/ComparisonPage"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const Betting = Loadable({
+  loader: () => import("../betting/index"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const ProfilePage = Loadable({
+  loader: () => import("../Profilepage/index"),
+  loading() {
+    return <Spinner />;
+  },
+});
+const Home = Loadable({
+  loader: () => import("../Main/mainPage"),
+  loading() {
+    return <Spinner />;
+  },
+});
 
-const Routes=(props)=>{
-  
-    useEffect(()=>{
-      let response = props.checkLoginStatus()
-      if(response.error){
-        //Error handeling
-      }
-    },[])
-
-    return(
-        <>
-          <RoutesContainer>
-          <GlobalStyle />  
-            <Switch>
-              <Layout>
-                <MainContainerDiv>
-                  <Route exact path="/" render={() => <Redirect to="/games" />}/>
-                  <Route path="/games" component={Games} />
-                  <Route path="/leaderboard" component={Leaderboard} />
-                  <Route path="/stats" component={Stats} />
-                  <Route path="/player/:player" component={Indiv} />
-                  <Route path="/team/:team" component={Indiv} />
-                  <Route path="/comparison" component={Comparsion} />
-                  <Route exact path="/terms-of-use" component={TermsOfUse} />
-                  <Route exact path="/privacy-policy" component={PrivacyPolicy}/>
-                  <Route path="/404" component={PageNotFound} />
-                  <Route path='/login' component={Login}/>
-                  <Route path='/betting' component={Betting}/>
-                  <Route path='/profile' component={ProfilePage}/>
-                  <Footer />
-                </MainContainerDiv>
-              </Layout>
-            </Switch>
-          </RoutesContainer>
-        </>
-    )
-}
-const mapStateToProps=(state)=>{
-    console.log(state)
-    return{
-      userDetails:state.authReducer.userDetails
+const Routes = (props) => {
+  useEffect(() => {
+    let response = props.checkLoginStatus();
+    if (response.error) {
+      //Error handeling
     }
   }, []);
-
   return (
     <>
       <RoutesContainer>
@@ -123,6 +92,7 @@ const mapStateToProps=(state)=>{
               <Route path="/404" component={PageNotFound} />
               <Route path="/login" component={Login} />
               <Route path="/betting" component={Betting} />
+              <Route path="/profile" component={ProfilePage} />
               <Footer />
             </MainContainerDiv>
           </Layout>
@@ -132,6 +102,7 @@ const mapStateToProps=(state)=>{
   );
 };
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
     userDetails: state.authReducer.userDetails,
   };
