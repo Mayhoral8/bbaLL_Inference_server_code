d2f7efb58fa68ca171b1480f7d34e06621c3772d
dustingank <chevalier.wyz@gmail.com>
2021-05-19 15:54:34 -0400

    Bug fixed: memory leak

--- code/src/Comparison/CompareSideInfo.js
+++ code/src/Comparison/CompareSideInfo.js
@@ -1,57 +1,62 @@
-import React, {useState}from 'react';
+import React, {useState, useEffect}from 'react';
 import "../fonts.css";
 import {StyledSideInfo} from "./StatisticsInformation-style";
 import { fbFirestore } from "../App/config";
 
 
-const CompareSideInfo = (props) => {
-    const currentYear = props.year;
-    const name = (props.name).replace(/ /g, "_").replace(/\./g, ",");
+const CompareSideInfo = ({name, year, isTeam, location}) => {
     const [resultArray, setResultArray] = useState(null);
-    var one, two, three;
+    var dataOne, dataTwo, dataThree;
 
-    fbFirestore.collection("player_basic_info")
-    .doc(name)
-    .get()
-    .then((doc)=>{
-        var weight = doc.data()["weight"][currentYear];
-        var height = doc.data()["height"][currentYear];
-        var salary = doc.data()["salary"][currentYear];
-        var temp = [];
-        temp.push(weight)
-        temp.push(height)
-        temp.push(salary)
-        setResultArray(temp)
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+    useEffect(() => {
+        if(year && name) {
+            getDatasFromFirestore();
+        }
+    }, [name, year])
 
-    if (resultArray != null) {
-        one =  resultArray[0];
-        two =  resultArray[1];
-        three =  resultArray[2];
-
-        three = three / 1000000;
-        three = three.toFixed(2);
+    function getDatasFromFirestore() {
+        fbFirestore.collection("player_basic_info")
+        .doc(name.replace(/ /g, "_").replace(/\./g, ","))
+        .get()
+        .then((doc)=>{
+            var weight = doc.data()["weight"][year];
+            var height = doc.data()["height"][year];
+            var salary = doc.data()["salary"][year];
+            var temp = [];
+            temp.push(weight)
+            temp.push(height)
+            temp.push(salary)
+            setResultArray(temp)
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
 
+    if (resultArray != null) {
+        dataOne =  resultArray[0];
+        dataTwo =  resultArray[1];
+        dataThree =  resultArray[2];
+
+        dataThree = dataThree / 1000000;
+        dataThree = dataThree.toFixed(2);
+    }
     //console.log("Within CompareSideInfo: " + currentYear + " " + name);
     return (
         <>
-        {props.isTeam == "false" ? (
-            <StyledSideInfo location = {props.location}>
+        {isTeam == "false" ? (
+            <StyledSideInfo location = {location}>
                 <div className={"info"}>
                     <span>Weight(kg)</span>
-                    <span>{one}</span>
+                    <span>{dataOne}</span>
                     <span>Height(cm)</span>
-                    <span>{two}</span>
+                    <span>{dataTwo}</span>
                     <span>Salary($mln)</span>
-                    <span>{three}</span>
+                    <span>{dataThree}</span>
                 </div>
             </StyledSideInfo>
             ) : (
-            <StyledSideInfo location = {props.location}>    
+            <StyledSideInfo location = {location}>    
                 <div className={"info"}>
                     <span>Elo Score</span>
                     <span>20</span>
