da8923056e13ba62f64c25260d21d42bed4c15c2
dustingank <chevalier.wyz@gmail.com>
2021-05-20 12:03:49 -0400

    re-write the versus style

---
+++ code/src/Comparison/customizeVersus.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+import { breakpoints } from '../constants/breakpoints'
+import thunder from '../assets/images/Thunder.png'
+import circle from '../assets/images/Circle.png'
+
+const CustomizeVersus = () => {
+  return (
+    <Wrapper>
+      <ComparisonVS>VS</ComparisonVS>
+    </Wrapper>
+  )
+}
+
+const ComparisonVS = styled.div`
+  margin:${({ center }) => center ? '0 12rem' : '0 4.5rem'};
+  padding: 1rem;
+  margin-top: 25%;
+  position: relative;
+  height: auto;
+
+  background-image: url(${circle});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100% 100%;
+
+  @media (max-width: ${breakpoints.phone}) {
+    font-size: 0.7rem;
+    margin: 0 1rem;
+    padding: 0.7rem;
+    margin-top: 5%;
+  }
+`
+
+const Wrapper = styled.div`
+  height: 100%;
+  background-image: url(${thunder});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: auto 100%;
+  
+`
+
+export default CustomizeVersus
