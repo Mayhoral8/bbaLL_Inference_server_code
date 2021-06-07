38176573432096a3e142eb0638c089c8f385b27b
Taha <tahanaeem12@hotmail.com>
2021-06-07 17:44:32 -0400

    removed comments

--- code/src/Main/memeCard.js
+++ code/src/Main/memeCard.js
@@ -21,45 +21,7 @@ const MemeCard = ({ urls }) => {
   };
 
   return (
-    // <Card>
-    // <div
-    //   style={{
-    //     display: "flex",
-    //     flexDirection: "row",
-    //     justifyContent: "space-between",
-    //     width: "100%",
-    //   }}
-    // >
-    //   <button className="button prev" onClick={prevbuttonHandle}>
-    //     <span>Previous</span>
-    //   </button>
-    //   <div
-    //     style={{
-    //       display: "flex",
-    //       justifyContent: "center",
-    //       alignItems: "center",
-    //       fontSize: "1.5rem",
-    //     }}
-    //   >
-    //     Highlights
-    //   </div>
-    //   <button className="button next" onClick={nextbuttonHandle}>
-    //     <span>Next</span>
-    //   </button>
-    // </div>
-
-    //   <Styles>
-    //     <div className="player-wrapper">
-    //       <ReactPlayer
-    //         className="react-player"
-    //         url={urls[currentVid]}
-    //         width="100%"
-    //         height="100%"
-    //       />
-    //     </div>
-    //   </Styles>
-    // </Card>
-    <Card1>
+    <Card>
       <div
         className="top"
         style={{
@@ -95,7 +57,7 @@ const MemeCard = ({ urls }) => {
             Try again later.
           </div>
         ) : (
-          <Styles>
+          <VideoPlayer>
             <div className="player-wrapper">
               <ReactPlayer
                 className="react-player"
@@ -105,14 +67,14 @@ const MemeCard = ({ urls }) => {
                 controls={true}
               />
             </div>
-          </Styles>
+          </VideoPlayer>
         )}
       </div>
-    </Card1>
+    </Card>
   );
 };
 
-export const Card1 = styled.div`
+export const Card = styled.div`
   display: flex;
   flex-direction: column;
 
@@ -230,7 +192,7 @@ export const Card1 = styled.div`
   }
 `;
 
-export const Styles = styled.div`
+const VideoPlayer = styled.div`
   .player-wrapper {
     position: relative;
     padding-top: 56.25%; /* Player ratio: 100 / (1280 / 720) */
@@ -241,93 +203,6 @@ export const Styles = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-  }
-`;
-
-export const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  // justify-content: flex-start;
-  // align-items: center;
-
-  height: 100%;
-
-  max-height: 495px;
-
-  background: white;
-  border: solid gray 1px;
-
-  //margin: 0rem 0rem auto 0rem;
-  padding: 0rem 1rem 1rem 1rem;
-
-  .button {
-    transition-duration: 0.4s;
-    background-color: white;
-    border: solid #555555 2px;
-    border-radius: 5px;
-    color: black;
-
-    padding: 1rem;
-    margin: 1rem 0rem 1rem 0rem;
-
-    text-align: center;
-    display: inline-block;
-    font-size: 16px;
-
-    transition-duration: 0.4s;
-    cursor: pointer;
-    min-width: 120px;
-  }
-
-  .button span {
-    cursor: pointer;
-    display: inline-block;
-    position: relative;
-    transition: 0.5s;
-  }
-
-  .next span:after {
-    font-size: 1.5rem;
-    content: "\\00bb";
-    position: absolute;
-    opacity: 0;
-    top: -7px;
-    right: -20px;
-    transition: 0.5s;
-  }
-
-  .prev span:before {
-    font-size: 1.5rem;
-    content: "\\00bb";
-    transform: rotate(180deg);
-    position: absolute;
-    opacity: 0;
-    top: -3px;
-    left: -75px;
-    transition: 0.5s;
-  }
-
-  .next:hover span {
-    padding-right: 20px;
-  }
-
-  .prev:hover span {
-    padding-left: 20px;
-  }
-
-  .button:hover span:after {
-    opacity: 1;
-    right: 0;
-  }
-
-  .button:hover span:before {
-    opacity: 1;
-    right: 0;
-  }
-
-  .button:hover {
-    background-color: #555555;
-    color: white;
   }
 `;
 
