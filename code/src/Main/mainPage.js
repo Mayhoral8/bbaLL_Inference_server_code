f84b792a7ae17f7fedbce87899cdf45b97b2ee43
Taha <tahanaeem12@hotmail.com>
2021-06-07 19:03:17 -0400

    refactor

--- code/src/Main/mainPage.js
+++ code/src/Main/mainPage.js
@@ -5,14 +5,13 @@ import { useSelector } from "react-redux";
 import { FullWidthMain } from "../globalStyles";
 import EventList from "./eventlist";
 import SEO from "../Shared/SEO";
-import FutureGameOddsCard from "./futureGameOddsCard";
 import { fbFirestore } from "../App/config";
 import useWindowSize from "Shared/hooks/useWindowSize";
 import PlayerRankingsCard from "./playerRankingsCard";
 import MemeCard from "./memeCard";
 import TeamScoreTable from "./TeamScoreTable";
 import styled from "styled-components";
-import { autoPercentage } from "chartjs-plugin-watermark";
+import FutureGameList from "./futuregamelist";
 
 const GamePageContainer = () => {
   const [data, setData] = useState([{}, {}, {}]);
@@ -91,32 +90,13 @@ const GamePageContainer = () => {
 
   const getFirebaseData = () => {
     let data = [];
-    // const documents = [
-    //   "bidaily_Top10",
-    //   "weekly_Top10",
-    //   "seasonal_Top10",
-    //   "team_Top10",
-    // ];
-    // const collections = {
-    //   bidaily_Top10: [
-    //     "FantasyScore",
-    //     "Points",
-    //     "PointsPerPoss",
-    //     "Three-Pointers",
-    //   ],
-    //   weekly_Top10: ["FantasyScore", "Points", "PointsPerPoss", "Three-Pointers"],
-    //   seasonal_Top10: ["Num_DD", "Num_TD"],
-    //   team_Top10: ["Massey Rating", "ELO Rating", "Standing"],
-    // };
-
-    const collections = ["player_ranking", "team_ranking"];
-
     fbFirestore
       .collection("ranking")
       .get()
       .then((snapshot) => {
         const documents = snapshot.docs.map((doc) => doc.data());
         const types = ["bidaily", "weekly", "seasonal"];
+
         let foundTypes = [];
         types.forEach((type) => {
           if (
@@ -163,135 +143,126 @@ const GamePageContainer = () => {
           gamePbp={gamePbp}
           gamePlayers={gamePlayers}
         />
-        <div style={{ margin: "0rem 0rem 0rem 0rem" }}>
-          <MainPageContainer>
-            <div style={{ display: "flex", flexDirection: "column" }}>
-              {useWindowSize() < 1400 && games.length > 0 && (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    backgroundColor: "white",
-                    boxShadow: "0px 1px 6px rgba(0, 0, 0, 0.08)",
-                    width: "100%",
-                  }}
-                >
-                  <div
-                    style={{
-                      margin: "1rem 0rem 0rem 0rem",
-                      fontSize: "1.5rem",
-                    }}
-                  >
-                    Upcoming Games
-                  </div>
-                  <div
-                    style={{
-                      zIndex: 0,
-                      marginTop: "0rem",
-                      marginLeft: "0rem",
-                      marginRight: "0rem",
-                      height: "100%",
-                      width: "100%",
-                      display: "flex",
-                      flexDirection: "row",
-                      padding: "0.5rem",
-                      overflowX: "scroll",
-                      overflowY: "hidden",
-                      scrollbarWidth: "thin" /* "auto" or "thin" */,
-                      // scrollbarColor: "#8783A8 #9693ab" /* scroll thumb and track */,
-                    }}
-                  >
-                    {games.map((item, index) => {
-                      return <FutureGameOddsCard data={item} key={index} />;
-                    })}
-                  </div>
-                </div>
-              )}
 
-              <RowContainer>
-                {hasDataLoaded ? (
-                  <PlayerRankingsCard
-                    data={[data[0], data[1], data[2]]}
-                    rankingTypes={playerRankingTypes}
-                  />
-                ) : (
-                  <div
-                    style={{
-                      background: "white",
-                      boxShadow: "0px 1px 6px rgba(0, 0, 0, 0.08)",
-                      height: "100%",
-                      width: "100%",
-                      margin: "0rem 3rem 3rem 0rem",
-                      minWidth: "400px",
-                    }}
-                  >
-                    <div style={{ fontSize: "1.5rem", padding: "1.5rem" }}>
-                      Player Rankings
-                    </div>
-                  </div>
-                )}
-                <MemeCard urls={memeUrls} />
-              </RowContainer>
-
-              <TeamRankingsContainer>
-                <div
-                  style={{
-                    fontSize: "1.5rem",
-                  }}
-                >
-                  NBA Team Rankings
-                </div>
-                {hasDataLoaded ? (
-                  <TeamScoreTable leftColHeading={"Rank"} data={data[3]} />
-                ) : (
-                  <div style={{ minHeight: "400px" }}></div>
-                )}
-              </TeamRankingsContainer>
-            </div>
-
-            {useWindowSize() > 1400 && games.length > 0 && (
+        <MainPageContainer>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {useWindowSize() < 1400 && games.length > 0 && (
               <div
                 style={{
-                  backgroundColor: "white",
-                  marginTop: "0rem",
-                  marginLeft: "3rem",
-                  height: "100%",
-                  minWidth: "300px",
                   display: "flex",
                   flexDirection: "column",
-                  padding: "0.5rem",
-                  overflowY: "scroll",
-                  overflowX: "hidden",
-                  position: "relative",
+                  alignItems: "center",
+                  backgroundColor: "white",
                   boxShadow: "0px 1px 6px rgba(0, 0, 0, 0.08)",
-                  height: "1043px",
-                  scrollbarWidth: "thin" /* "auto" or "thin" */,
-                  // scrollbarColor: "#8783A8 #9693ab" /* scroll thumb and track */,
+                  width: "100%",
                 }}
               >
+                <FutureGameTitle>Upcoming Games</FutureGameTitle>
                 <div
                   style={{
+                    zIndex: 0,
+                    marginTop: "0rem",
+                    marginLeft: "0rem",
+                    marginRight: "0rem",
+                    height: "100%",
+                    width: "100%",
                     display: "flex",
-                    justifyContent: "center",
-                    alignContent: "center",
-                    fontSize: "1.5rem",
-                    margin: "1rem 0rem 1rem 0rem",
+                    flexDirection: "row",
+                    padding: "0.5rem",
+                    overflowX: "scroll",
+                    overflowY: "hidden",
+                    scrollbarWidth: "thin" /* "auto" or "thin" */,
+                    // scrollbarColor: "#8783A8 #9693ab" /* scroll thumb and track */,
                   }}
                 >
-                  Upcoming Games
+                  <FutureGameList games={games} />
                 </div>
-                {games.map((item, index) => {
-                  return <FutureGameOddsCard data={item} key={index} />;
-                })}
               </div>
             )}
-          </MainPageContainer>
-        </div>
+
+            <RowContainer>
+              {hasDataLoaded ? (
+                <PlayerRankingsCard
+                  data={[data[0], data[1], data[2]]}
+                  rankingTypes={playerRankingTypes}
+                />
+              ) : (
+                <PlayerRankingPlaceholderBox>
+                  <PlayerRankingsPlaceholderTitle>
+                    Player Rankings
+                  </PlayerRankingsPlaceholderTitle>
+                </PlayerRankingPlaceholderBox>
+              )}
+              <MemeCard urls={memeUrls} />
+            </RowContainer>
+
+            <TeamRankingsContainer>
+              <TeamRankingsTitle>NBA Team Rankings</TeamRankingsTitle>
+              {hasDataLoaded ? (
+                <TeamScoreTable leftColHeading={"Rank"} data={data[3]} />
+              ) : (
+                <div style={{ minHeight: "400px" }}></div>
+              )}
+            </TeamRankingsContainer>
+          </div>
+
+          {useWindowSize() > 1400 && games.length > 0 && (
+            <FutureGameListBox>
+              <FutureGameTitle>Upcoming Games</FutureGameTitle>
+              <FutureGameList games={games} />
+            </FutureGameListBox>
+          )}
+        </MainPageContainer>
       </FullWidthMain>
     </>
   );
 };
+
+const FutureGameListBox = styled.div`
+  background-color: white;
+  margin-top: 0rem;
+  margin-left: 3rem;
+  height: 100%;
+  min-width: 300px;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  position: relative;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.08);
+  height: 1043px;
+  scrollbar-width: thin; /* "auto" or "thin" */
+`;
+
+const PlayerRankingsPlaceholderTitle = styled.div`
+  font-size: 1.5rem;
+  padding: 1.5rem;
+`;
+
+const PlayerRankingPlaceholderBox = styled.div`
+  background: white;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.08);
+  height: 100%;
+  width: 100%;
+  margin: 0rem 3rem 3rem 0rem;
+  min-width: 400px;
+`;
+
+const TeamRankingsTitle = styled.div`
+  font-size: 1.5rem;
+`;
+
+const FutureGameTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  font-size: 1.5rem;
+  @media (max-width: 1400px) {
+    margin: 1rem 0rem 0rem 0rem;
+  }
+  margin: 1rem 0rem 1rem 0rem;
+`;
 
 const TeamRankingsContainer = styled.div`
   display: flex;
