7408ce3e0ca6aece6576ae4735021fa1ec87c588
Jiwoong Im <jiwoongim@b-10-27-86-45.dynapool.vpn.nyu.edu>
2021-06-10 13:46:07 -0400

    frontend warning issue gone

--- code/src/betting/Shared/userStatsContainer.js
+++ code/src/betting/Shared/userStatsContainer.js
@@ -1,164 +1,136 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 //Actions
-import {getUserRecord} from '../../redux/actions/recordActions'
-import {LogoutAction} from "../../redux/actions/authActions";
-
+import { getUserRecord } from "../../redux/actions/recordActions";
+import { LogoutAction } from "../../redux/actions/authActions";
 
 //Components
 import {
-    UserStatsContainer, 
-    ProfileImgFiguresViewMore, 
-    ProfileImg, 
-    UserName, 
-    PointsRank,
-    LevelWinRate,
-    Stats,
-    StatsContainer,
-    ViewMoreLink,
-    IconFigure,
-    Icon,
-    Figure,
-    LoginLogoutBtnsContainer,
-    LoginLogoutBtnsWrapper,
-    AuthBtn
-} from './userStatsContainerStyles'
+  UserStatsContainer,
+  ProfileImgFiguresViewMore,
+  ProfileImg,
+  UserName,
+  PointsRank,
+  LevelWinRate,
+  Stats,
+  StatsContainer,
+  ViewMoreLink,
+  IconFigure,
+  Icon,
+  Figure,
+  LoginLogoutBtnsContainer,
+  LoginLogoutBtnsWrapper,
+  AuthBtn,
+} from "./userStatsContainerStyles";
 
 //Images
-import Img from '../../assets/images/avatar.jpg'
-import starIcon from '../../assets/images/star.svg'
-import levelIcon from '../../assets/images/level.svg'
-import rankIcon from '../../assets/images/rank.svg'
-import winRateIcon from '../../assets/images/winRate.svg'
-import loginIcon from '../../assets/images/loginIcon.png'
-import logoutIcon from '../../assets/images/logoutIcon.png'
-
+import Img from "../../assets/images/avatar.jpg";
+import starIcon from "../../assets/images/star.svg";
+import levelIcon from "../../assets/images/level.svg";
+import rankIcon from "../../assets/images/rank.svg";
+import winRateIcon from "../../assets/images/winRate.svg";
+import loginIcon from "../../assets/images/loginIcon.png";
+import logoutIcon from "../../assets/images/logoutIcon.png";
 
 //Functions and libraries
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {ClipLoader} from 'react-spinners'
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 const UserStatsBox = (props) => {
-    const [statsSpinner, setStatsSpinner] = useState(true)
-    const [winningRate, setWinningRate] = useState(0)
-    useEffect(() => {
-        props.getUserRecord(props.userDetails.uid)
-    },[])
+  const [statsSpinner, setStatsSpinner] = useState(true);
+  const [winningRate, setWinningRate] = useState(0);
+  useEffect(() => {
+    props.getUserRecord(props.userDetails.uid);
+  }, []);
 
-    useEffect(() => {
-        if(props.userRecord.level){
-            let calWinningRate = props.userRecord.numBettings === 0 ? 0
-            :
-            props.userRecord.numWins / props.userRecord.numBettings
-            setWinningRate(calWinningRate)
-            setStatsSpinner(false)
-        }
-    }, [props.userRecord])
+  useEffect(() => {
+    if (props.userRecord.level) {
+      let calWinningRate =
+        props.userRecord.numBettings === 0
+          ? 0
+          : props.userRecord.numWins / props.userRecord.numBettings;
+      setWinningRate(calWinningRate);
+      setStatsSpinner(false);
+    }
+  }, [props.userRecord]);
 
-    return(
-        <UserStatsContainer>
-            <UserName>{props.userDetails.displayName}</UserName>
-            <ProfileImgFiguresViewMore>
-                <ProfileImg src={Img}/>
-                <StatsContainer>
-                    {
-                        props.userRecord.level
-                        ?
-                        <>
-                            <PointsRank>
-                                <Stats>
-                                    Points
-                                    <IconFigure>
-                                        <Icon src = {starIcon}/>
-                                        <Figure>
-                                            {props.userRecord.totalPoints}
-                                        </Figure>
-                                    </IconFigure>
-                                </Stats>
+  return (
+    <UserStatsContainer>
+      <UserName>{props.userDetails.displayName}</UserName>
+      <ProfileImgFiguresViewMore>
+        <ProfileImg src={Img} />
+        <StatsContainer>
+          {props.userRecord.level ? (
+            <>
+              <PointsRank>
+                <Stats>
+                  Points
+                  <IconFigure>
+                    <Icon src={starIcon} />
+                    <Figure>{props.userRecord.totalPoints.toFixed(3)}</Figure>
+                  </IconFigure>
+                </Stats>
 
-                                <Stats>
-                                    Rank
-                                    <IconFigure>
-                                        <Icon src = {rankIcon}/>
-                                        <Figure>
-                                            {props.userRecord.rank}
-                                        </Figure>
-                                    </IconFigure>
-                                </Stats>
-                            </PointsRank>
+                <Stats>
+                  Rank
+                  <IconFigure>
+                    <Icon src={rankIcon} />
+                    <Figure>{props.userRecord.rank}</Figure>
+                  </IconFigure>
+                </Stats>
+              </PointsRank>
 
-                            <LevelWinRate>
-                                <Stats>
-                                    Level
-                                    <IconFigure>
-                                        <Icon src = {levelIcon}/>
-                                        <Figure>
-                                            {props.userRecord.level}
-                                        </Figure>
-                                    </IconFigure>
-                                </Stats>
+              <LevelWinRate>
+                <Stats>
+                  Level
+                  <IconFigure>
+                    <Icon src={levelIcon} />
+                    <Figure>{props.userRecord.level}</Figure>
+                  </IconFigure>
+                </Stats>
 
-                                <Stats>
-                                    Win Rate
-                                    <IconFigure>
-                                        <Icon src = {winRateIcon}/>
-                                        <Figure>
-                                            {winningRate.toFixed(2)}
-                                        </Figure>
-                                    </IconFigure>
-                                </Stats>
-                            </LevelWinRate>
-                        </>
-                        :
-                        <ClipLoader color = '#C4C4C4' size = '' loading = {statsSpinner}/>
-                    }
+                <Stats>
+                  Win Rate
+                  <IconFigure>
+                    <Icon src={winRateIcon} />
+                    <Figure>{winningRate.toFixed(2)}</Figure>
+                  </IconFigure>
+                </Stats>
+              </LevelWinRate>
+            </>
+          ) : (
+            <ClipLoader color="#C4C4C4" size="" loading={statsSpinner} />
+          )}
 
-                    {
-                        props.userRecord.level 
-                        ? 
-                        <ViewMoreLink>
-                            <Link
-                            to = '/profile'
-                            >
-                                View More...
-                            </Link>
-                        </ViewMoreLink>
-                        :
-                        null
-                    }
+          {props.userRecord.level ? (
+            <ViewMoreLink>
+              <Link to="/profile">View More...</Link>
+            </ViewMoreLink>
+          ) : null}
 
-                    {
-                        props.userDetails.displayName
-                        ?
-                        <LoginLogoutBtnsContainer>
-                            <LoginLogoutBtnsWrapper onClick = {() => {
-                            props.userDetails.uid ? onLogoutClick() : setLoginModalVisible(true)
-                            }}>
-                                {
-                                    props.userDetails.uid 
-                                    ? 
-                                    <AuthBtn 
-                                    src = {logoutIcon}
-                                    />
-                                    :
-                                    <AuthBtn
-                                    src = {loginIcon}
-                                    />
-                                }
-                                
-                            </LoginLogoutBtnsWrapper>
-                        </LoginLogoutBtnsContainer>
-                        :
-                        null 
-                    }
-
-                </StatsContainer>
-            </ProfileImgFiguresViewMore>
-        </UserStatsContainer>
-    )
-}
-
+          {props.userDetails.displayName ? (
+            <LoginLogoutBtnsContainer>
+              <LoginLogoutBtnsWrapper
+                onClick={() => {
+                  props.userDetails.uid
+                    ? onLogoutClick()
+                    : setLoginModalVisible(true);
+                }}
+              >
+                {props.userDetails.uid ? (
+                  <AuthBtn src={logoutIcon} />
+                ) : (
+                  <AuthBtn src={loginIcon} />
+                )}
+              </LoginLogoutBtnsWrapper>
+            </LoginLogoutBtnsContainer>
+          ) : null}
+        </StatsContainer>
+      </ProfileImgFiguresViewMore>
+    </UserStatsContainer>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -167,4 +139,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {getUserRecord})(UserStatsBox)
+export default connect(mapStateToProps, { getUserRecord })(UserStatsBox);
