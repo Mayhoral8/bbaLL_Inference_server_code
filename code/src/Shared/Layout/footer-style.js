f34922f0d95d06b87b7b96e602220181a466c653
dustingank <chevalier.wyz@gmail.com>
2021-06-30 12:54:47 -0400

    Change footer color

--- code/src/Shared/Layout/footer-style.js
+++ code/src/Shared/Layout/footer-style.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { darken } from 'polished';
 
 export const FooterDiv = styled.div`
-  background: var(--penblue);
+  background: var(--main-purple);
   color: var(--faint-purple);
   padding: 4rem 3rem 0;
   .footer-width {
