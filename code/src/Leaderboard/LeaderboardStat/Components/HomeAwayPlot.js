2c1813ad7629d2b299b2818d20adeb518653cedb
Steve Song <steve.b.song92@gmail.com>
2021-10-14 21:18:23 -0400

    FE: Leaderboard and Stats bars replaced with chartJS

--- code/src/Leaderboard/LeaderboardStat/Components/HomeAwayPlot.js
+++ code/src/Leaderboard/LeaderboardStat/Components/HomeAwayPlot.js
@@ -5,188 +5,240 @@ import { useHistory } from "react-router-dom";
 import useWindowSize from "Shared/hooks/useWindowSize";
 import logo from "Assets/images/new-logo-square.png";
 import { HomeAwayGraphDiv, HomeAwayYAxis } from "./homeawayplot-style";
-import { ABB2TEAM, GRAPH_COLOR, MOBILE_SM_BREAKPOINT } from 'Constants';
+import { ABB2TEAM, GRAPH_COLOR, MOBILE_SM_BREAKPOINT } from "Constants";
+import { Scatter, Chart } from "react-chartjs-2";
+import "chartjs-plugin-datalabels";
 
 const HomeAwayPlot = ({ x, y, names, xaxis, yaxis, teamColours }) => {
   const windowSize = useWindowSize();
   const history = useHistory();
-  const isTeam = useSelector(state => state.sidebarReducer.isTeam);
-
+  const isTeam = useSelector((state) => state.sidebarReducer.isTeam);
+  const min = Math.floor(Math.min(...x, ...y));
+  const max = Math.ceil(Math.max(...x, ...y));
   // define graph layout variables
+
   let data, margin, yAxisTitle, axisFontSize, tickFontSize;
   let markerFontSize = 16;
   const graphDashColor = "#534a91";
 
-
   const minOrMaxRange = (range, dividend) => {
-    if (range === 'min') {
-      return (Math.min(
-        Math.min.apply(Math, x) - (Math.max.apply(Math, x) - Math.min.apply(Math, x)) / dividend,
-        Math.min.apply(Math, y) - (Math.max.apply(Math, y) - Math.min.apply(Math, y)) / dividend
-      ))
+    if (range === "min") {
+      return Math.min(
+        Math.min.apply(Math, x) -
+          (Math.max.apply(Math, x) - Math.min.apply(Math, x)) / dividend,
+        Math.min.apply(Math, y) -
+          (Math.max.apply(Math, y) - Math.min.apply(Math, y)) / dividend
+      );
     }
 
-    return (Math.max(
-      Math.max.apply(Math, x) + (Math.max.apply(Math, x) - Math.min.apply(Math, x)) / dividend,
-      Math.max.apply(Math, y) + (Math.max.apply(Math, y) - Math.min.apply(Math, y)) / dividend
-    ))
-  }
-
-  // control graph layout depending on the browser width
-  if (windowSize < MOBILE_SM_BREAKPOINT) {
-    margin = {
-      l: 20,
-      r: 20,
-      b: 40,
-      t: 10,
-    };
-    yAxisTitle = "";
-    axisFontSize = 12;
-    tickFontSize = 12;
-  } else {
-    margin = {
-      l: 80,
-      r: 50,
-      b: 60,
-      t: 10,
-    };
-    yAxisTitle = yaxis;
-    axisFontSize = 20;
-    tickFontSize = 16;
-  }
-
-  // logo config
-  const images = [
-    {
-      source: logo,
-      xref: "paper",
-      yref: "paper",
-      x: 1,
-      y: 0.05,
-      sizex: 0.7,
-      sizey: 0.2,
-      opacity: 0.1,
-      layer: "below",
-      xanchor: "right",
-      yanchor: "bottom",
-    },
-  ];
-
-  // layout
-  const layout = {
-    showlegend: false,
-    colorway: [],
-    images,
-    autosize: true,
-    margin,
-    font: {
-      family: 'Roboto Condensed'
-    },
-    xaxis: {
-      fixedrange: true,
-      title: {
-        text: xaxis,
-      },
-      titlefont: {
-        size: axisFontSize,
-        color: GRAPH_COLOR
-      },
-      tickfont: {
-        size: tickFontSize,
-        color: GRAPH_COLOR
-      },
-      range: [
-        minOrMaxRange('min', 5),
-        minOrMaxRange('max', 5)
-      ],
-    },
-    yaxis: {
-      fixedrange: true,
-      title: {
-        text: yAxisTitle,
-      },
-      tickfont: {
-        size: tickFontSize,
-        color: GRAPH_COLOR
-      },
-      titlefont: {
-        size: axisFontSize,
-        color: GRAPH_COLOR
-      },
-      range: [
-        minOrMaxRange('min', 5),
-        minOrMaxRange('max', 4)
-      ],
-    },
-  };
-
-  const config = {
-    displayModeBar: false,
-    responsive: true,
-    staticPlot: false,
-  }
-
-  // data config
-  const namesAbbr = names.map(name => Object.keys(ABB2TEAM).find(key => ABB2TEAM[key] === name));
-  const dataText = namesAbbr.map(name => '<b>' + name + '</b>');
-  if (isTeam) {
-    data = [
-      {
-        x: x,
-        y: y,
-        text: dataText,
-        link: namesAbbr,
-        textfont: {
-          color: teamColours,
-          size: markerFontSize,
-        },
-        type: "scatter",
-        mode: "text",
-      },
-    ];
-  } else {
-    data = [
-      {
-        x: x,
-        y: y,
-        text: names.map((name) => name.replace(/\s/g, "<br>")),
-        link: names,
-        textfont: {
-          color: teamColours,
-          size: markerFontSize,
-        },
-        type: "scatter",
-        mode: "text",
-      },
-    ];
-  }
-
-  data.push({
-    mode: "lines",
-    name: "lines",
-    type: "scatter",
-    x: [0, 1000],
-    y: [0, 1000],
-    line: {
-      color: graphDashColor,
-      dash: "dash",
-    },
-  });
-
-  // handle click event on data points
-  const handleClickData = (e) => {
-    const selectedName = e.points[0].data.link[e.points[0].pointIndex]
-    const link = isTeam ? ABB2TEAM[selectedName].replaceAll(" ", "_") : selectedName.replaceAll(" ", "_");
-    history.push(
-      `/${isTeam ? "team" : "player"}/${link}`
+    return Math.max(
+      Math.max.apply(Math, x) +
+        (Math.max.apply(Math, x) - Math.min.apply(Math, x)) / dividend,
+      Math.max.apply(Math, y) +
+        (Math.max.apply(Math, y) - Math.min.apply(Math, y)) / dividend
     );
   };
 
+  // handle click event on data points
+
+  // const handleClickData = (e) => {
+  //   const selectedName = e.points[0].data.link[e.points[0].pointIndex];
+  //   const link = isTeam
+  //     ? ABB2TEAM[selectedName].replaceAll(" ", "_")
+  //     : selectedName.replaceAll(" ", "_");
+  //   history.push(`/${isTeam ? "team" : "player"}/${link}`);
+  // };
+  console.log(
+    "x: ",
+    x.length,
+    "y: ",
+    y.length,
+    "names: ",
+    names,
+    "xais, : ",
+    xaxis,
+    "yaxis: ",
+    yaxis,
+    "yAxisTitle: ",
+    yAxisTitle
+  );
+
+  // preprocess scatter points data [{x: , y: }...]
+
+  const scatterPoints = [];
+  for (let i = 0; i <= x.length - 1; i++) {
+    const yVal = y[i].toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+    const xVal = x[i].toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+
+    const name = names[i];
+    scatterPoints.push({
+      label: name,
+      x: xVal,
+      y: yVal,
+    });
+  }
+  // line
+  const line = [];
+  const b = 1;
+  for (let i = min; i <= max; i++) {
+    // if (i === 0) x[i] = min;
+    // if (i === x.length - 1) x[i] = max;
+    line.push({ label: "", x: i, y: i });
+  }
+  console.log("line-->", line);
+
+  console.log("scatterPoints: ", scatterPoints);
+  const scatterChartData = {
+    datasets: [
+      {
+        type: "scatter",
+        data: scatterPoints,
+        backgroundColor: teamColours,
+        borderColor: teamColours,
+        borderWidth: 1,
+        fill: false,
+      },
+      {
+        type: "line",
+        data: line,
+        borderWidth: 0,
+        fill: false,
+        labels: false,
+        borderDash: [5, 10],
+        pointRadius: 0,
+        // borderColor: rgba(210, 77, 255),
+        borderColor: "rgb(76, 19, 173)",
+        borderWidth: 2,
+        font: {
+          size: 30,
+        },
+      },
+    ],
+  };
+
+  const legend = {
+    display: false,
+    responsive: true,
+  };
+
+  const scatterOptions = {
+    responsive: true,
+    spanGaps: true,
+    scales: {
+      yAxes: [
+        {
+          // type: "linear",
+          // position: "bottom",
+          scaleLabel: {
+            display: true,
+            labelString: "Away",
+            fontSize: 20,
+          },
+          ticks: {
+            min,
+            max,
+            // min: minOrMaxRange("min", 5),
+            // max: minOrMaxRange("max", 5),
+            autoSkip: true,
+            fontSize: 14,
+            userCallback: function (label, index, labels) {
+              // when the floored value is the same as the value we have a whole number
+              if (Math.floor(label) === label) {
+                return label;
+              }
+            },
+          },
+        },
+      ],
+      xAxes: [
+        {
+          // type: "linear",
+          // position: "bottom",
+          scaleLabel: {
+            display: true,
+            labelString: "Home",
+            fontSize: 20,
+          },
+          ticks: {
+            min,
+            max,
+            // min: minOrMaxRange("min", 5),
+            // max: minOrMaxRange("max", 5),
+            // labels: scatterPoints.map((data) => {
+            //   return data.x;
+            // }),
+            autoSkip: true,
+            // beginAtZero: true,
+            fontSize: 14,
+            userCallback: function (label, index, labels) {
+              // when the floored value is the same as the value we have a whole number
+              if (Math.floor(label) === label) {
+                return label;
+              }
+            },
+          },
+          // gridLines: {
+          //   display: true,
+          // },
+        },
+      ],
+    },
+    plugins: {
+      datalabels: {
+        display: function (label, index) {
+          console.log("label,==>", label);
+        },
+        align: "top",
+        anchor: "right",
+        color: "black",
+        padding: { right: 10 },
+      },
+    },
+    tooltips: {
+      position: "nearest",
+      mode: "point",
+      intersect: true,
+      yPadding: 10,
+      xPadding: 10,
+      caretSize: 18,
+      caretPadding: 10,
+      backgroundColor: "white",
+      titleFontColor: "#000",
+      bodyFontColor: "#000",
+      borderColor: "rgba(0,0,0,1)",
+      borderWidth: 1,
+      yAlign: "bottom",
+    },
+    // Core options
+    aspectRatio: 5 / 3,
+    layout: {
+      padding: {
+        top: 32,
+        right: 16,
+        bottom: 16,
+        left: 8,
+      },
+    },
+  };
+  // const options = {
+  //   scales: {
+  //     xAxes: {
+  //       type: "linear",
+  //       position: "bottom",
+  //     },
+  //     yAxes: {
+  //       type: "linear",
+  //       position: "bottom",
+  //     },
+  //   },
+  // };
   return (
     <>
-      <HomeAwayYAxis>{windowSize < MOBILE_SM_BREAKPOINT ? yaxis : ""}</HomeAwayYAxis>
+      <HomeAwayYAxis>
+        {windowSize < MOBILE_SM_BREAKPOINT ? yaxis : ""}
+      </HomeAwayYAxis>
       <HomeAwayGraphDiv isTeam={isTeam}>
-        <Plot
+        {/* <Plot
           useResizeHandler
           data={data}
           onClick={(e) => handleClickData(e)}
@@ -194,6 +246,11 @@ const HomeAwayPlot = ({ x, y, names, xaxis, yaxis, teamColours }) => {
           className="plot"
           style={{ width: "100%", height: "60vh" }}
           layout={layout}
+        /> */}
+        <Scatter
+          data={scatterChartData}
+          legend={legend}
+          options={scatterOptions}
         />
       </HomeAwayGraphDiv>
     </>
